617d6bcfda8e2971fdab76abb7e606ab1ef05146
Mohamed Arshad <mohamedarshadcholasseri5050@gmail.com>
2020-05-30 20:29:56 +0530

    All images and layout set and new controller created name : HomeController

---
+++ app/Http/Controllers/HomeConroller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeConroller extends Controller
+{
+   public function index(){
+
+        return view('pages.home_content');
+
+
+   }
+
+
+}
+
